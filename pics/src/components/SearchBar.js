0e39e36efa3bf53636aab2574973ea87b50eb5aa
RynuRen <pros0327@gmail.com>
2024-02-11 22:52:49 +0900

    add: Add input value control

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,5 +1,28 @@
-function SearchBar() {
-    return <div>SearchBar</div>;
+import { useState } from "react";
+
+function SearchBar({ onSubmit }) {
+  // Put input value under state sysmte control
+  // DO NOT REACH DIRECTLY INPUT VALUE
+  const [term, setTerm] = useState("");
+
+  const handleFormSubmit = (event) => {
+    // Prevent default html form action
+    event.preventDefault();
+
+    onSubmit(term);
+  };
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input value={term} onChange={handleChange} />
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
