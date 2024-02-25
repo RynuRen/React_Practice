e2ee62aecea9f3ad3ca606f28286a9802c50be03
RynuRen <pros0327@gmail.com>
2024-02-25 13:09:44 +0900

    add: Add update books state

--- books/src/App.js
+++ books/src/App.js
@@ -5,11 +5,18 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log("Need to add book with:", title);
+    // State system MUST point different NEW object address!!
+    // https://state-updates.vercel.app/
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title },
+    ];
+    setBooks(updatedBooks);
   };
 
   return (
     <div>
+      {books.length}
       <BookCreate onCreate={createBook} />
     </div>
   );
