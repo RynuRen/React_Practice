047086f6baf720b6ae7b410a07bcb37e23c0ab8b
RynuRen <pros0327@gmail.com>
2024-02-25 13:26:10 +0900

    add: Add remove book func

--- books/src/App.js
+++ books/src/App.js
@@ -5,6 +5,14 @@ import BookList from "./components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  };
+
   const createBook = (title) => {
     // State system MUST point different NEW object address!!
     // https://state-updates.vercel.app/
@@ -12,12 +20,13 @@ function App() {
       ...books,
       { id: Math.round(Math.random() * 9999), title },
     ];
+
     setBooks(updatedBooks);
   };
 
   return (
     <div className="app">
-      <BookList books={books} />
+      <BookList books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
