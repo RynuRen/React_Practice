75746ea4ee277ad364015751f9687fe79e1b2d41
RynuRen <pros0327@gmail.com>
2024-03-24 17:19:09 +0900

    add: Add icons, pass through props in Button

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,19 +1,26 @@
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
 import Button from "./Button";
+
 function App() {
   return (
     <div>
       <div>
         <Button success rounded outline>
+          <GoBell />
           Click here!!
         </Button>
       </div>
       <div>
         <Button danger outline>
+          <GoCloudDownload />
           Buy Now!
         </Button>
       </div>
       <div>
-        <Button warning>See Deal!</Button>
+        <Button warning>
+          <GoDatabase />
+          See Deal!
+        </Button>
       </div>
       <div>
         <Button secondary outline>
