e9d1d2d7eadde0ced6395088b762d63ba1c82d1e
RynuRen <pros0327@gmail.com>
2024-04-20 20:07:23 +0900

    add: Add dropdown prop system

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,27 +1,13 @@
-import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 
 function App() {
-  const items = [
-    {
-      id: "123",
-      label: "Can I use React on a project?",
-      content:
-        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
-    },
-    {
-      id: "456",
-      label: "Can I use Javascript on a project?",
-      content:
-        "You can use Javascript on any project you want. You can use Javascript on any project you want. You can use Javascript on any project you want.",
-    },
-    {
-      id: "789",
-      label: "Can I use CSS on a project?",
-      content:
-        "You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want.",
-    },
+  const options = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
   ];
-  return <Accordion items={items} />;
+
+  return <Dropdown options={options} />;
 }
 
 export default App;
---
+++ comps/src/pages/AccordionPage.js
@@ -0,0 +1,27 @@
+import Accordion from "../components/Accordion";
+
+function AccordionPage() {
+  const items = [
+    {
+      id: "123",
+      label: "Can I use React on a project?",
+      content:
+        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
+    },
+    {
+      id: "456",
+      label: "Can I use Javascript on a project?",
+      content:
+        "You can use Javascript on any project you want. You can use Javascript on any project you want. You can use Javascript on any project you want.",
+    },
+    {
+      id: "789",
+      label: "Can I use CSS on a project?",
+      content:
+        "You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want.",
+    },
+  ];
+  return <Accordion items={items} />;
+}
+
+export default AccordionPage;
