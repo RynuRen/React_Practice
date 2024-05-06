4fe7bd641898916af500a795530a953d4487db48
RynuRen <pros0327@gmail.com>
2024-05-06 12:07:16 +0900

    add: Add back and forward click listener

--- comps/src/context/navigation.js
+++ comps/src/context/navigation.js
@@ -1,8 +1,21 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const NavigationContext = createContext();
 
-function NavigationProvider() {
+function NavigationProvider({ children }) {
+  const [currnetPath, setCurrnetPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => {
+      setCurrnetPath(window.location.pathname);
+    };
+    window.addEventListener("popstate", handler);
+
+    return () => {
+      window.removeEventListener("popstate", handler);
+    };
+  }, []);
+
   return (
     <NavigationContext.Provider value={{}}>
       {children}
