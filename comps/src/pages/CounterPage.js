1a1d3896322dc3ccbcc210562daabf6c241968dc
RynuRen <pros0327@gmail.com>
2024-06-30 16:57:30 +0900

    add: Add new state updates

--- comps/src/pages/CounterPage.js
+++ comps/src/pages/CounterPage.js
@@ -3,7 +3,9 @@ import Button from "../components/Button";
 import Panel from "../components/Panel";
 
 const INCREMENT_COUNT = "increment";
+const DECREMENT_COUNT = "decrement";
 const SET_VALUE_TO_ADD = "set-value-to-add";
+const ADD_VALUE_TO_COUNT = "add-value-to-count";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -12,28 +14,26 @@ const reducer = (state, action) => {
         ...state,
         count: state.count + 1,
       };
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
     case SET_VALUE_TO_ADD:
       return {
         ...state,
         valueToAdd: action.payload,
       };
+    case ADD_VALUE_TO_COUNT:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0,
+      };
     default:
       // throw new Error("unexpected action type: " + action.type);
       return state;
   }
-  // if (action.type === INCREMENT_COUNT) {
-  //   return {
-  //     ...state,
-  //     count: state.count + 1,
-  //   };
-  // }
-  // if (action.type === SET_VALUE_TO_ADD) {
-  //   return {
-  //     ...state,
-  //     valueToAdd: action.payload,
-  //   };
-  // }
-  // return state;
 };
 
 function CounterPage({ initialCount }) {
@@ -49,12 +49,13 @@ function CounterPage({ initialCount }) {
     });
   };
   const decrement = () => {
-    // setCount(count - 1);
+    dispatch({
+      type: DECREMENT_COUNT,
+    });
   };
 
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
-
     dispatch({
       type: SET_VALUE_TO_ADD,
       payload: value,
@@ -62,8 +63,9 @@ function CounterPage({ initialCount }) {
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    // setCount(count + valueToAdd);
-    // setValueToAdd(0);
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    });
   };
 
   return (
